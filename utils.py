9490f81b7f59ceb589949bde16f1ae4c6f9b5405
Prashal Ruchiranga <prashalruchiranga@gmail.com>
2024-03-23 20:36:29 +0530

    Fix

--- utils.py
+++ utils.py
@@ -5,8 +5,9 @@ from copy import deepcopy
 
 def annotshow(image, annotation, radius=5, tl_color=(255, 0, 0), br_color=(0, 0, 255), thickness=-1):
     image_copy = deepcopy(image)
-    for dic in annotation:
-        x0, y0, width, height = [int(x) for x in dic['bbox']]
+    for point in annotation:
+        point = [int(x) for x in point]
+        x0, y0, width, height = point
         cv2.circle(image_copy, (x0, y0), radius, tl_color, thickness)
         cv2.circle(image_copy, (x0+width, y0+height), radius, br_color, thickness)
     fig, ax = plt.subplots(1)
