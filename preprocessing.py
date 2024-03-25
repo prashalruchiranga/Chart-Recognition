5fa61fc2f9151d918beabfb19879f1bb21482e1b
Prashal Ruchiranga <prashalruchiranga@gmail.com>
2024-03-25 17:10:15 +0530

    Add make_heatmaps

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,8 @@ import json
 from copy import deepcopy
 import operator
 import cv2
+import tensorflow as tf
+import numpy as np
 
 def clean(file):
     '''
@@ -38,9 +40,11 @@ def scale(annotation, input_shape, output_shape):
     c, d = output_shape
     sx = d / b
     sy = c / a
+    foo = lambda x, y: np.round(operator.mul(x, y)).astype(np.int32)
     for dic in deep_copy:
         x0, y0, width, height = dic['bbox']
-        dic['bbox'] = list(map(operator.mul, dic['bbox'], [sy, sx, sy, sx]))
+        dic['bbox'] = list(map(foo, dic['bbox'], [sy, sx, sy, sx]))
+        
     return deep_copy
 
 
@@ -59,3 +63,90 @@ def transform(annotation, input_shape, output_shape):
         dic['bbox'] = [x0, y0, x0+width, y0+height]
     return deep_copy
 
+
+def generate_2d_guassian(height, width, y0, x0, sigma=1, scale=12):
+    """
+    "The same technique as Tompson et al. is used for supervision. A MeanSquared Error (MSE) loss is
+    applied comparing the predicted heatmap to a ground-truth heatmap consisting of a 2D gaussian
+    (with standard deviation of 1 px) centered on the keypoint location."
+
+    https://github.com/princeton-vl/pose-hg-train/blob/master/src/util/img.lua#L204
+
+    This code generates a 2D Gaussian heatmap centered at a specified point (y0, x0) in a grid of size height by width. 
+    The Gaussian is defined by its standard deviation sigma, and its scale is adjusted by the scale parameter. 
+    This heatmap is used for supervision in training neural networks, particularly for tasks like pose estimation.
+    
+    """
+    heatmap = tf.zeros((height, width))
+
+    # this gaussian patch is 7x7, let's get four corners of it first
+    xmin = x0 - 3 * sigma
+    ymin = y0 - 3 * sigma
+    xmax = x0 + 3 * sigma
+    ymax = y0 + 3 * sigma
+    # if the patch is out of image boundary we simply return nothing according to the source code
+    # [1]"In these cases the joint is either truncated or severely occluded, so for
+    # supervision a ground truth heatmap of all zeros is provided."
+    if xmin >= width or ymin >= height or xmax < 0 or ymax < 0:
+        return heatmap
+
+    size = 6 * sigma + 1
+    x, y = tf.meshgrid(tf.range(0, 6*sigma+1, 1), tf.range(0, 6*sigma+1, 1), indexing='xy')
+
+    # the center of the gaussian patch should be 1
+    center_x = size // 2
+    center_y = size // 2
+
+    # generate this 7x7 gaussian patch
+    gaussian_patch = tf.cast(tf.math.exp(-(tf.square(x - center_x) + tf.math.square(y - center_y)) / (tf.math.square(sigma) * 2)) * scale, dtype=tf.float32)
+
+    # part of the patch could be out of the boundary, so we need to determine the valid range
+    # if xmin = -2, it means the 2 left-most columns are invalid, which is max(0, -(-2)) = 2
+    patch_xmin = tf.math.maximum(0, -xmin)
+    patch_ymin = tf.math.maximum(0, -ymin)
+    # if xmin = 59, xmax = 66, but our output is 64x64, then we should discard 2 right-most columns
+    # which is min(64, 66) - 59 = 5, and column 6 and 7 are discarded
+    patch_xmax = tf.math.minimum(xmax, width) - xmin
+    patch_ymax = tf.math.minimum(ymax, height) - ymin
+
+    # also, we need to determine where to put this patch in the whole heatmap
+    heatmap_xmin = tf.math.maximum(0, xmin)
+    heatmap_ymin = tf.math.maximum(0, ymin)
+    heatmap_xmax = tf.math.minimum(xmax, width)
+    heatmap_ymax = tf.math.minimum(ymax, height)
+
+    # finally, insert this patch into the heatmap
+    indices = tf.TensorArray(tf.int32, 1, dynamic_size=True)
+    updates = tf.TensorArray(tf.float32, 1, dynamic_size=True)
+
+    count = 0
+
+    for j in tf.range(patch_ymin, patch_ymax):
+        for i in tf.range(patch_xmin, patch_xmax):
+            indices = indices.write(count, [heatmap_ymin+j, heatmap_xmin+i])
+            updates = updates.write(count, gaussian_patch[j][i])
+            count += 1
+                
+    heatmap = tf.tensor_scatter_nd_update(heatmap, indices.stack(), updates.stack())
+
+    # unfortunately, the code below doesn't work because 
+    # tensorflow.python.framework.ops.EagerTensor' object does not support item assignment
+    # heatmap[heatmap_ymin:heatmap_ymax, heatmap_xmin:heatmap_xmax] = gaussian_patch[patch_ymin:patch_ymax,patch_xmin:patch_xmax]
+
+    return heatmap
+
+
+def make_heatmaps(keypoint_x, keypoint_y):
+    # Define SCALE as heatmap.shape[0] / image.shape[0] 
+    # SCALE = 64 / 256
+    SCALE = tf.cast(0.25, dtype=tf.float32)
+    x = tf.cast(tf.math.round(keypoint_x * SCALE), dtype=tf.int32) 
+    y = tf.cast(tf.math.round(keypoint_y * SCALE), dtype=tf.int32) 
+    count = len(x)
+    heatmap_array = tf.TensorArray(tf.float32, count)
+    for i in range(count):
+        gaussian = generate_2d_guassian(64, 64, y[i], x[i])
+        heatmap_array = heatmap_array.write(i, gaussian)
+    heatmaps = heatmap_array.stack()
+    heatmaps = tf.transpose(heatmaps, perm=[1, 2, 0]) # change to (64, 64, 16)
+    return heatmaps
